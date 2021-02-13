f05bc30a11a4d8ff3f221058be583ad97604bd31
Alexandre Amor <dev@ralex.fr>
2021-02-13 19:21:38 +0100

    Add documentation with bootstrap script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Test
+# salt-masterless
+Saltstack top file and states for my masterless setup.
+
+## Bootstrap a masterless machine
+
+```bash
+curl https://github.com/ralex/salt-masterless/bootstrap.sh -o bootstrap.sh
+chmod +x bootstrap.sh
+./bootstrap.sh
+```
+
+## Documentation
+
+|Repository|Content|
+|---|---|
+|`salt-masterless`|(This repository) Contains states to manage a masterless setup|
+|[`salt-latop`](https://github.com/ralex/salt-laptop)| Provides states for a work station setup|
+
+
+![Diagram](docs/diagram.svg)
+
+*`salt-pillar` is a private git repository as it may contain sensitive informations.*
---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Bootstrap Saltstack
+curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
+sudo sh bootstrap_salt.sh -v -X -p python3-git stable
+
+# Set masterless minion
+echo 'file_client: local' | sudo tee /etc/salt/minion.d/file_client.conf
+
+# Configure 
+sudo tee /etc/salt/minion.d/fileserver.conf << EOF
+fileserver_backend:
+  - git
+
+file_ignore_regex:
+  - ^\.git
+
+gitfs_remotes:
+  - https://github.com/ralex/salt-laptop
+
+EOF
+
+echo 'Enter roles separated with comma (",") or press enter to skip grains setup.'
+read grains
+
+if [ ! -z $grains ]; then
+    echo 'roles:' | sudo tee /etc/salt/grains
+    for grain in $(echo $grains | sed "s/,/ /g"); do
+        echo "  - ${grain}" | sudo tee -a /etc/salt/grains
+    done
+fi
+
+sudo salt-call --local state.apply
